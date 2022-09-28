35df5611f37943e1b57e8688bd39b8b990b4bcdf
Divyanshu <divyanshu.it19@nsut.ac.in>
2022-09-29 02:21:28 +0530

    Added dummy git IPFS functionality, along with accesing who did what and all the other data

--- test/Fir.test.js
+++ test/Fir.test.js
@@ -3,63 +3,95 @@ const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 const compile  = require("../compile");
+const axios = require('axios').default;
 
 const interface= compile.interface;
 const bytecode= compile.bytecode;
 
 let accounts;
-let FIR;
+let FIR1;
+let FIR2;
 
 beforeEach(async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
-  FIR = await new web3.eth.Contract(JSON.parse(interface))
+  FIR1 = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({
       data: bytecode,
       arguments: ["Rahim","Abdul","Karim","Boriwali","Mobile Snatching","387","3234"],
     })
     .send({ from: accounts[0], gas: "1000000" });
+
+  FIR2 = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Ram","Shiva","Karim","Boriwali","Mobile Snatching","45","3234"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+  
 });
 
-describe("FIR", () => {    
+describe("FIR1", () => {    
   it("deploys a contract", () => {
-    // console.log(FIR);
-    assert.ok(FIR.options.address);
+    // console.log(FIR1);
+    assert.ok(FIR1.options.address);
+    async function getLog(){
+      try{
+        data= await axios.get("https://api.github.com/repos/Divyanshu2411/FIRdata/commits")
+        console.log(data.data[0].commit.url);
+        let indiCommit= data.data[0].commit.url.split('/').at(-1);
+        console.log("Individual commit ID of last commit", indiCommit);
+        try{
+          let indiData=await axios.get("https://api.github.com/repos/Divyanshu2411/FIRdata/commits/"+indiCommit);
+          console.log(indiData.data.files)
+        }
+        catch(error){
+          console.log(error);
+        }
+        
+      }
+      catch(error){
+        console.log(error.message)
+    };
+    }
+    getLog();
+    
+    
   });
 
   it("Recording officer", async()=>{
-    const officer= await FIR.methods.recordingOfficer().call();
-    assert("Rahim",officer);
-    // console.log(await FIR.methods.recordingOfficer().call()); //this is how you access the asynced data
+    const officer= await FIR1.methods.recordingOfficer().call();
+    assert.equal("Rahim",officer);
+    console.log(await FIR1.methods.recordingOfficer().call()); //this is how you access the asynced data
   });
   it("Recording Person", async()=>{
-    const recPerson= await FIR.methods.recordingPerson().call();
-    assert("Abdul",recPerson);
+    const recPerson= await FIR1.methods.recordingPerson().call();
+    assert.equal("Abdul",recPerson);
   });
 
   it("complainant", async()=>{
-    const complainer= await FIR.methods.complainant().call();
-    assert("Karim",complainer);
+    const complainer= await FIR1.methods.complainant().call();
+    assert.equal("Karim",complainer);
   });
 
   it("Police Station", async()=>{
-    const PS= await FIR.methods.policeStation().call();
-    assert("Boriwali",PS);
+    const PS= await FIR1.methods.policeStation().call();
+    assert.equal("Boriwali",PS);
   });
   
   it("Offence", async()=>{
-    const crime= await FIR.methods.offence().call();
-    assert("Mobile Snatching",crime);
+    const crime= await FIR1.methods.offence().call();
+    assert.equal("Mobile Snatching",crime);
   });
 
   it("IPC section", async()=>{
-    const sec= await FIR.methods.section().call();
-    assert("387",sec);
+    const sec= await FIR1.methods.section().call();
+    assert.equal("387",sec);
   });
 
   it("Police Station Code", async()=>{
-    const PSC= await FIR.methods.policeStationCode().call();
-    assert("3234",PSC);
+    const PSC= await FIR1.methods.policeStationCode().call();
+    assert.equal("3234",PSC);
   });
 
 });
